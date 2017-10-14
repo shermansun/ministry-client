3e2fcec9a495efb81638d94c701ddc9783ef3e92
Sherman Sun mini <sherman_sun@yahoo.com.hk>
2017-10-15 00:03:45 +1100

    roster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RosterComponent } from './components/roster/roster.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'angular-calendar';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
+const appRouter:Routes = [
+  {path:'', component:RosterComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RosterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forRoot(appRouter),
+    NgbModule.forRoot(),
+    NgbModalModule.forRoot(),
+    BrowserAnimationsModule,
+    CalendarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
